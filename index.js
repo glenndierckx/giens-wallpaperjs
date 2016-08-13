e9b3888c64693644baa40eba5810021ecce47fed
Glenn Dierckx <glenn@windstyle.be>
2016-08-13 22:10:07 +0200

    Live wallpaper

---
+++ index.js
@@ -0,0 +1,19 @@
+const wallpaper = require('wallpaper');
+var http = require('http');
+var fs = require('fs');
+var request = require('request');
+var socket = require('socket.io-client')('http://glenndierckx.be/');
+
+socket.on("new-bg", function (data) {
+  var date = new Date(data.date);
+  console.log("New bg img! " + date);
+  var writeStream = fs.createWriteStream("file.jpg");
+  writeStream.on('finish', function () {
+    wallpaper.set("file.jpg");
+    console.log("done");
+  });
+  var imgurl = "http://glenndierckx.be/dynbg/" + date.getTime() + ".jpg";
+  request(imgurl).pipe(writeStream);
+});
+
+process.stdin.resume();
